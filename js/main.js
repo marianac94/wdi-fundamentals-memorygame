49d9ba0b028621a17d88eab96e7f3fedf9cf8632
Mariana Castro <marianacastro@Marianas-MacBook-Pro-2.local>
2018-08-28 23:50:07 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardFour);
+var cardsInPlay = [];
+
+var cardOne = ["queen"];
+cardsInPlay.push('queen');
+console.log("User flipped queen");
+
+var cardTwo = ["king"];
+cardsInPlay.push('king');
+console.log("User flipped king");
+
+var cardThree = ["queen"];
+cardsInPlay.push('queen');
+console.log("User flipped queen");
+
+var cardFour = ["king"];
+cardsInPlay.push('king');
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
