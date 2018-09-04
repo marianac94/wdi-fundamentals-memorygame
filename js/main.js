cb7f8acb778490c1d0e5a1fb20426873c3af96bf
Mariana Castro <marianacastro@Marianas-MacBook-Pro-2.local>
2018-09-03 19:25:59 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+
+
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen", 
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king", 
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 
 var cardsInPlay = [];
+	// var cardOne = cards[0];
+	// var cardTwo = cards[2];
+	// var cardThree = cards[1];
+	// var cardFour = cards[3];
 
-// var cardOne = cards[0];
-// var cardTwo = cards[2];
-// var cardThree = cards[1];
-// var cardFour = cards[3];
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
   checkForMatch();
 };
+
 
 var checkForMatch = function() {
  if (cardsInPlay.length > 1) {
@@ -27,5 +54,5 @@ var checkForMatch = function() {
 };
 
 
-flipCard(2);
 flipCard(0);
+flipCard(2);
